c5a0ded2f8cb29fd15ec56e778bd7ef965b80eed
arunak451 <aruarun142004@gmail.com>
2026-03-28 12:57:57 +0530

    update backend URL

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -337,7 +337,7 @@ function App() {
     }
     setLoading(true);
     try {
-      const response = await fetch('http://127.0.0.1:8000/contact', {
+      const response = await fetch('https://xplore-it-backend.onrender.com/contact', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData)
